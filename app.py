11a2139c4a0ecc659a7b4bab245c0df37edde000
17093 <17093@burnside.school.nz>
2020-05-29 11:47:53 +1200

    the

--- app.py
+++ app.py
@@ -1,8 +1,11 @@
 #Video sorting application made by Kevin Kang
-from flask import Flask, render_template, g, request, redirect, url_for
+from flask import Flask, render_template, g, request, redirect, url_for, session
 import sqlite3
+from datetime import timedelta
 
 app = Flask(__name__)
+app.secret_key = "bread"
+app.permanent_session_lifetime = timedelta(seconds=10)
 
 DATABASE = 'urlstorage.db'
 
@@ -20,33 +23,35 @@ def get_db():
         db = g._database = sqlite3.connect(DATABASE)
     return db
 
+#default route
 @app.route('/')
 def index():
     return render_template('index.html')
 
+#login route
 @app.route('/login', methods=['GET','POST'])
 def login():
-    logged = False 
+    session.pop("user", None)
     error = None
     if request.method == "POST":
         username=request.form.get("username")
         password=request.form.get("password")
-        print (username, password)
-
-
         cursor = get_db().cursor()
         #gets cursor and compares the credentials
         find_user = ("SELECT * FROM users WHERE username = ? AND password = ?")
         cursor.execute(find_user,[(username),(password)])
         results = cursor.fetchall()
-
-        print("results")
         
+        #if username and password match the database's credentials,
+        #it will redirect to dashboard
         if results:
-            logged = True
+            session["user"] = username
             return redirect(url_for("dashboard"))
+        #if user is already logged in, it will redirect to dashboard.
+        #if not, it will reload login.html.
         else:
-            logged = False
+            if "user" in session:
+                return redirect(url_for("dashboard"))
     return render_template("login.html")
 
 
@@ -57,8 +62,18 @@ def signup():
 
 @app.route('/dashboard')
 def dashboard():
+    if "user" in session:
+        user = session["user"]
+        return f"<h1> logged in as {user}</h1>"
     
     return render_template('dashboard.html')
+    
+#will logout users and redirect to login page.
+@app.route('/logout')
+def logout():
+    session.pop("user", None)
+    return redirect(url_for("login"))
+    
 
 if __name__ == '__main__' :
     app.run(debug=True)
